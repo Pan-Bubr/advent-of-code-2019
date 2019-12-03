931f8cd845c4bd4c01cfc4f97aa032f099972166
Pan Bubr <19240387+Pan-Bubr@users.noreply.github.com>
2019-12-03 23:21:22 +0100

    Day 3 in Python - part 2

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ After hitting a wall, I'll try to continue using Typescript - my main language.
 | Day | Name | Rust | Python | Typescript | 
 |:---:|:---|:---:|:---:|:---:|
 | 01 | [The Tyranny of the Rocket Equation][day01] | ✓✓ | ✓✓ | |
-| 01 | [1202 Program Alarm][day02] |  | ✓✓ | |
+| 01 | [1202 Program Alarm][day02] | ✓✓ | ✓✓ | |
+| 01 | [Crossed Wires][day03] |  | ✓✓ | |
 
 # Test Solutions
 
--- python/d3.py
+++ python/d3.py
@@ -18,23 +18,41 @@ class Instruction:
         self.direction = instruction[0]
         self.steps = int(instruction[1:])
 
+class Step:
+    def __init__(self, x, y, A_steps, B_steps):
+        self.x = int(x)
+        self.y = int(y)
+        self.A_steps = A_steps
+        self.B_steps = B_steps
+    
+    def manhattan(self):
+        return abs(self.x) + abs(self.y)
+    
+    def step_count(self):
+        return self.A_steps + self.B_steps
+
 class Plane:
-    manhattan = 99999
+    steps = 0
+    crossings = []
     def __init__(self, size):
         self.size = size
-        self.plane = [[0 for y in range(size)] for x in range(size)]
+        self.plane = [[None for y in range(size)] for x in range(size)]
     
     def print(self):
         for i in self.plane:
             print(i)
 
     def draw_cell(self, x, y, mark):
-        
-        if self.plane[y][x] != 0 and self.plane[y][x] != mark:
-            manhattan = abs(x - self.size/2) + abs(y - self.size/2)
-            if manhattan < self.manhattan:
-                self.manhattan = manhattan
-        self.plane[y][x] = mark
+        self.steps += 1
+        if mark == "A":
+            if self.plane[y][x] == None:
+                self.plane[y][x] = Step(x - self.size/2, y - self.size/2, self.steps, 0)
+        elif mark == "B":
+            if self.plane[y][x] == None:
+                self.plane[y][x] = Step(x - self.size/2, y - self.size/2, 0, self.steps)
+            elif self.plane[y][x].A_steps != 0:
+                self.plane[y][x].B_steps = self.steps
+                self.crossings.append(self.plane[y][x])
     
     def draw_line(self, i, mark):
         for _ in range(i.steps):
@@ -47,13 +65,13 @@ class Plane:
             elif i.direction == "D":
                 self.pointer.y += 1
             self.draw_cell(self.pointer.x, self.pointer.y, mark)
-            
 
     def draw_wire(self, instructions, mark):
+        self.steps = 0
+        self.manhattan = 0
         self.pointer = Pointer(int(self.size/2), int(self.size/2))
         for i in instructions:
             self.draw_line(i, mark)
-
 
 
 # Part 1
@@ -69,4 +87,13 @@ plane = Plane(25000)
 plane.draw_wire(instructionListA, "A")
 plane.draw_wire(instructionListB, "B")
 
-print("[Part One] Manhattan distance of closed wire crossing:", int(plane.manhattan))
+min_manhattan = 999999
+min_step_count = 999999
+for c in plane.crossings:
+    if c.manhattan() < min_manhattan:
+        min_manhattan = c.manhattan()
+    if c.step_count() < min_step_count:
+        min_step_count = c.step_count()
+
+print("[Part One] Min. manhattan distance to crossing is:", min_manhattan)
+print("[Part Two] Min. step count to crossing is:", min_step_count)
